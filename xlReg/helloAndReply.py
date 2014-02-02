d14d44b2ac1098d4b538a8fa6cdfc71d8b649c4c
Jim Dixon <jddixon@gmail.com>
2014-02-01 17:23:43 -0800

    edited error constants

--- xlReg/helloAndReply.py
+++ xlReg/helloAndReply.py
@@ -13,12 +13,18 @@ def clientEncryptHello(version, ck):
     data = bytearray(56)
     rnglib.NextBytes(data)
 
-    # append the version, a DecimalVersion, to the array
+    # append the version, a DecimalVersion, to the array.  Should be
+    # able to use
+    #    msg.extend(chunk)
+    # or something similar, where chunk is version seen as a bytearray 
+
     # XXX STUB
 
     pubKey = str(ck).encode('utf8')
     bio = BIO.MemoryBuffer(pubKey) 
     rsa = RSA.load_pub_key_bio(bio)
+
+    # pcsk#1 with oaep sometimes called pkcs#1 v2.0
     ciphertext = rsa.public_encrypt(data, RSA.pkcs1_oaep_padding)
     return ciphertext
 
