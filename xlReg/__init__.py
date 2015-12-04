44d155ed7e7a668230b288fdf9145a841dc9e108
Jim Dixon <jddixon@gmail.com>
2015-12-04 11:52:05 -0800

    minor edits to *.md

--- xlReg/__init__.py
+++ xlReg/__init__.py
@@ -1,8 +1,9 @@
 # xlReg/__init__.py
 
-__version__      = '0.1.10'
-__version_date__ = '2015-10-14'
+__version__      = '0.1.11'
+__version_date__ = '2015-12-04'
 
 __all__ = ['AES_BLOCK_SIZE',]
 
 AES_BLOCK_SIZE = 16
+
