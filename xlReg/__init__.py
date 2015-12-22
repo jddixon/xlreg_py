fd7a6501938e09d18650832e1e6a8aa28d649e7d
Jim Dixon <jddixon@gmail.com>
2015-12-22 13:00:08 -0800

    fix dead link

--- xlReg/__init__.py
+++ xlReg/__init__.py
@@ -1,11 +1,12 @@
 # xlReg/__init__.py
 
-__version__      = '0.1.12'
-__version_date__ = '2015-12-10'
+__version__      = '0.1.13'
+__version_date__ = '2015-12-22'
 
 __all__ = ['__version__',   '__version_date__', 
            'AES_BLOCK_SIZE',]
 
 AES_BLOCK_SIZE = 16
+
 
 
