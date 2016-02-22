e1221247532ae2d2394048cafeb07bcb91b130aa
Jim Dixon <jddixon@gmail.com>
2016-02-22 14:25:44 -0800

    license update

--- xlReg/__init__.py
+++ xlReg/__init__.py
@@ -1,12 +1,13 @@
 # xlReg/__init__.py
 
-__version__      = '0.1.13'
-__version_date__ = '2015-12-22'
+__version__      = '0.1.14'
+__version_date__ = '2016-02-22'
 
 __all__ = ['__version__',   '__version_date__', 
            'AES_BLOCK_SIZE',]
 
 AES_BLOCK_SIZE = 16
+
 
 
 
