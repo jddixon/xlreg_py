fc66179e36c4fc82d5031f0ecb39a7c9dbbbfee3
Jim Dixon <jddixon@gmail.com>
2016-05-05 09:51:48 -0700

    add .dvcz/projCfg*

--- xlReg/__init__.py
+++ xlReg/__init__.py
@@ -1,7 +1,7 @@
 # xlReg/__init__.py
 
-__version__      = '0.1.15'
-__version_date__ = '2016-03-29'
+__version__      = '0.1.16'
+__version_date__ = '2016-05-05'
 
 __all__ = ['__version__', '__version_date__',
            'AES_BLOCK_SIZE', ]
