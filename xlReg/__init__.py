d0383baeaa5e8fd2f7d8f913bc681cf050ebd800
Jim Dixon <jddixon@gmail.com>
2016-06-11 15:27:06 -0700

    add .dvcz, bkp2U, other bash scripts

--- xlReg/__init__.py
+++ xlReg/__init__.py
@@ -1,10 +1,9 @@
 # xlReg/__init__.py
 
-__version__      = '0.1.16'
-__version_date__ = '2016-05-05'
+__version__ = '0.1.17'
+__version_date__ = '2016-06-11'
 
 __all__ = ['__version__', '__version_date__',
            'AES_BLOCK_SIZE', ]
 
 AES_BLOCK_SIZE = 16
-
